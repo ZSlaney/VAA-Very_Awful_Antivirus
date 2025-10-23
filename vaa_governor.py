8da32cbb75fafdb31e23ec3f0315fde5fb963dc0
Conleth <103609099@student.swin.edu.au>
2025-10-23 18:03:23 +1100

    server

--- vaa_governor.py
+++ vaa_governor.py
@@ -2,7 +2,18 @@
 from utils import hash_fns
 from utils import SQL_handler
 from utils import CreateUser
+import socket
 
 class VaaGovernor:
     def __init__(self):
         self.clients = [] # List to keep track of authenticated clients (sessionkey, name, permissions)
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.bind(('localhost', 7986))
+        self.serversocket.listen(5)  # Listen for incoming connections
+
+    def main_loop(self):
+        print("VAA Governor is running and listening for connections...")
+        while True:
+            clientsocket, address = self.serversocket.accept()
+            print(f"Connection from {address} has been established!")
+            # Here you would handle authentication and further communication
